b3b402819ae8d85f7a86a8c27d0a34ee60960261
Luke Marsden <me@lukemarsden.net>
2016-11-08 15:06:08 -0800

    fixup

--- setup.php
+++ setup.php
@@ -1,5 +1,5 @@
 <?php
-$dbconn = pg_connect("host=psql dbname=testdb user=testuser password=password");
+$dbconn = pg_connect("host=psql dbname=userdb user=testuser password=password");
 //pg_query("create database words");
 //$dbconn = pg_connect("host=psql dbname=words user=testuser password=password");
 pg_query("create table hello (word text)");
