46f3dd7afb57b68f89525bdb72d9f6c3d64e0af9
Luke Marsden <me@lukemarsden.net>
2016-11-08 12:58:25 -0800

    fixup

--- index.php
+++ index.php
@@ -16,20 +16,21 @@ if($color == "blue") {
 }
 ?>
 <div style="padding:1em; background-color: <?=$hex?>; color:white; text-align:center;">Hello, world! I am the <?=$color?> version, running in container <?=`hostname`?> in <?=json_decode(file_get_contents("http://freegeoip.net/json"))->country_name;?>.</div>
-<form action="index.php">
-<p>Word: <input type="text"></p>
+<form action="/" method="POST">
+<p>Word: <input type="text" name="word"></p>
 <input type="submit">
 </form>
 <?php
-$dbconn = pg_connect("host=postgres dbname=words user=root password=password");
-if($_POST["text"]) {
-    pg_query("insert into hello (name) values (".pg_escape_literal($_POST["text"]).")");
+$dbconn = pg_connect("host=postgres dbname=words user=demo password=mysecretpassword");
+if($_POST["word"]) {
+    pg_query("insert into hello (word) values (".pg_escape_literal($_POST["word"]).")");
+    ?>DONE<?
 }
 print pg_last_error();
 $result = pg_query("select word from hello");
 ?><ul><?
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    ?><li><?=$line?></li><?
+    ?><li><?=$line["word"]?></li><?
 }
 ?></ul>
 </body>
--- setup.php
+++ setup.php
@@ -1,3 +1,5 @@
 <?php
-$dbconn = pg_connect("host=pgsql dbname=words user=root password=password");
+$dbconn = pg_connect("host=postgres user=demo password=mysecretpassword");
+pg_query("create database words");
+$dbconn = pg_connect("host=postgres dbname=words user=demo password=mysecretpassword");
 pg_query("create table hello (word text)");
